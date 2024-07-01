ab635f6b41386c4ecf5148f86a0024e4e2e6f970
Maha Emad <mahaemadsaid224@gmail.com>
2024-07-01 15:28:22 +0300

    add decorations to item card

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -1,14 +1,16 @@
 import React from "react"; 
 import '../App.css';   
 import {Product} from './product' 
-
+import '../assets/productList.css'
 
 
 const ProductCard : React.FC<{ product: Product }> = ({ product })  =>{
 
-    return ( 
+    return (  
+        <> 
         <div className ='product-card'> 
-         <img  src={product.image}alt={product.name} className="product-image" />
+        <img  src= {product.image} alt={product.name} className="product-image" />
+
         <h3 className ='product-title'>{product.name}</h3>  
         <p className='product-details'>{product.description}</p>  
         <div className='product-price'>{product.price.value}{product.price.currancy}</div>      
@@ -16,7 +18,7 @@ const ProductCard : React.FC<{ product: Product }> = ({ product })  =>{
             <button >Add to Cart</button>
         </div>
         </div>
-        
+        </>
 
     ); 
 }
